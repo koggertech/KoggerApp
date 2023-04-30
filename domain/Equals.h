a0c22afd19449458b354535f1c7063f2dcb4b369
Kharlamenko <kharlamenkodev@gmail.com>
2023-04-30 16:44:44 +0300

    equal method has been implemented for long double type.

--- domain/Equals.h
+++ domain/Equals.h
@@ -22,6 +22,15 @@ equal(T x, T y, int ulp = 2)
         || fabs(x - y) < std::numeric_limits<double>::min();
 }
 
+template<class T>
+typename std::enable_if<std::is_same<T, long double>::value, bool>::type
+equal(T x, T y, int ulp = 2)
+{
+    return fabs(x - y) <= std::numeric_limits<long double>::epsilon() * fabs(x + y) * static_cast<long double>(ulp)
+        || fabs(x - y) < std::numeric_limits<long double>::min();
+}
+
+
 template<typename T>
 bool
 equal(const Edge<T>& e1, const Edge<T>& e2)
