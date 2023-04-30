c59d877612da9af4924ab805b721d62b3115e341
Kharlamenko <kharlamenkodev@gmail.com>
2023-04-30 16:25:45 +0300

    Hash function for point 3D has been moved to Point3D.h. Implemented contour vertex object getter.

--- Core/abstractprocessingcase.h
+++ Core/abstractprocessingcase.h
@@ -7,24 +7,18 @@
 #include "vertexobject.h"
 #include "Point3D.h"
 #include "sceneparams.h"
+#include "boundarydetector.h"
 
 class AbstractProcessingCase
 {
 public:
     virtual VertexObject process(const QVector <QVector3D>& bottomTrack, SceneParams params) = 0;
+
+    VertexObject contourVertexObject() const {return mContourVertexObject;};
+
+protected:
+
+    VertexObject mContourVertexObject;
 };
-
-//! Хешер для типа данных точки (необходим для устранения повторяющихся
-//! точек с помощью QSet при триангуляции)
-template <typename T>
-inline uint qHash(const Point3D <T> &p, uint seed = 0)
-{
-    Q_UNUSED(seed)
-
-    uint h1 = qHash(p.x());
-    uint h2 = qHash(p.y());
-    uint h3 = qHash(p.z());
-    return h1 ^ (h2 << 1) ^ (h3 << 2);
-}
 
 #endif // ABSTRACTPROCESSINGCASE_H
