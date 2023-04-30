99cd1de64aca082aa709c6559882f3474dd1455f
Kharlamenko <kharlamenkodev@gmail.com>
2023-04-30 17:01:30 +0300

    Process method has been refactored.

--- processors/onlytincase.cpp
+++ processors/onlytincase.cpp
@@ -7,34 +7,46 @@ OnlyTinCase::OnlyTinCase()
 
 VertexObject OnlyTinCase::process(const QVector<QVector3D> &bottomTrack, SceneParams params)
 {
-    // Удаляем повторяющиеся точки используя множество
-    QSet <Point3D <double>> set;
-
-    for (const auto& _p : bottomTrack){
-        Point3D <double> p(_p.x(), _p.y(), _p.z());
-        set.insert(p);
-    }
-
-    // Формируем вектор точек для триангуляции
+    std::set <Point2D <double>> set;
     std::vector <Point3D <double>> input;
 
-    for (const auto& _p : set){
-        Point3D <double> p;
-        p.setX(static_cast <double> (_p.x()));
-        p.setY(static_cast <double> (_p.y()));
-        p.setZ(static_cast <double> (_p.z()));
+    for (size_t i = 0; i < bottomTrack.size(); i++){
+        Point2D <double> point(static_cast <double> (bottomTrack.at(i).x()),
+                               static_cast <double> (bottomTrack.at(i).y()),
+                               static_cast <double> (i));
 
-        input.push_back(p);
+        set.insert(point);
     }
 
-    // Выполняем триангуляцию
-    Delaunay <double> triangulator;
+    for (const auto& p : set){
+        Point3D <double> point(p.x(),
+                               p.y(),
+                               static_cast <double> (bottomTrack.at(p.index()).z()),
+                               p.index());
+        input.push_back(point);
+    }
 
-    auto pTriangles = triangulator.trinagulate(input, params.triangulationEdgeLengthLimit());
+    Delaunay <double> delaunay;
+    auto triangles = delaunay.trinagulate(input, params.triangulationEdgeLengthLimit());
+
+    //DelaunayTin::BowyerWatson <double> delaunay;
+    //auto triangles = delaunay.build(input, params.triangulationEdgeLengthLimit());
+
+    BoundaryDetector <double> boundaryDetector;
+    auto boundary = boundaryDetector.detect(*triangles);
+
+    //auto boundary = BoundaryDetector <double>::simpleTinBoundary(delaunayResult);
+
+    mContourVertexObject.setPrimitiveType(GL_LINES);
+
+    for (const auto& edge : boundary){
+        mContourVertexObject.append({QVector3D(edge.p1().x(), edge.p1().y(), edge.p1().z()),
+                                     QVector3D(edge.p2().x(), edge.p2().y(), edge.p2().z())});
+    }
 
     // Формируем вершинный объект
     VertexObject object(GL_TRIANGLES);
-    for (const auto& t : *pTriangles){
+    for (const auto& t : *triangles){
         object.append(QVector3D(t.A().x(), t.A().y(), t.A().z()));
         object.append(QVector3D(t.B().x(), t.B().y(), t.B().z()));
         object.append(QVector3D(t.C().x(), t.C().y(), t.C().z()));
