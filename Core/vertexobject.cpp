30cf97ef0afaef53a0762092c9d4dfd37b148fb7
Kharlamenko <kharlamenkodev@gmail.com>
2023-05-27 13:04:27 +0300

    vertex object class has been refactored.

--- Core/vertexobject.cpp
+++ Core/vertexobject.cpp
@@ -26,30 +26,21 @@ void VertexObject::setData(const QVector<QVector3D> &data)
 {
     mData = data;
 
-    if (data.isEmpty())
-        return;
-
-    mMaximumZ = data.first().z();
-    mMinimumZ = mMaximumZ;
-
-    for (const auto& v: data){
-        mMinimumZ = std::min(mMinimumZ, v.z());
-        mMaximumZ = std::max(mMaximumZ, v.z());
-    }
-
-
+    createBounds();
 }
 
 void VertexObject::append(const QVector3D &vertex)
 {
     mData.append(vertex);
+
+    createBounds();
 }
 
 void VertexObject::append(const QVector<QVector3D> &vertexVector)
 {
     mData.append(vertexVector);
 
-    //updateGrid();
+    createBounds();
 }
 
 int VertexObject::primitiveType() const
@@ -75,5 +66,40 @@ float VertexObject::maximumZ() const
 float VertexObject::minimumZ() const
 {
     return mMinimumZ;
+}
+
+Cube VertexObject::bounds() const
+{
+    return mBounds;
+}
+
+void VertexObject::createBounds()
+{
+    if (mData.isEmpty()){
+        mBounds = Cube(0.0f, 0.0f, 0.0f,0.0f,0.0f,0.0f);
+        return;
+    }
+
+    float z_max = mData.first().z();
+    float z_min = z_max;
+
+    float x_max = mData.first().x();
+    float x_min = x_max;
+
+    float y_max = mData.first().y();
+    float y_min = y_max;
+
+    for (const auto& v: mData){
+        z_min = std::min(z_min, v.z());
+        z_max = std::max(z_max, v.z());
+
+        x_min = std::min(x_min, v.x());
+        x_max = std::max(x_max, v.x());
+
+        y_min = std::min(y_min, v.y());
+        y_max = std::max(y_max, v.y());
+    }
+
+    mBounds = Cube(x_min, x_max, y_min, y_max, z_min, z_max);
 }
 
--- Core/vertexobject.h
+++ Core/vertexobject.h
@@ -12,6 +12,46 @@
 
 #include <GL/gl.h>
 
+class Cube{
+
+public:
+    Cube(float x_1, float x_2,
+             float y_1, float y_2,
+             float z_1, float z_2)
+    :mXMin(x_1), mXMax(x_2)
+    ,mYMin(y_1), mYMax(y_2)
+    ,mZMin(z_1), mZMax(z_2)
+    {}
+
+    float minimumX() const {return mXMin;}
+    float maximumX() const {return mXMax;}
+    float minimumY() const {return mYMin;}
+    float maximumY() const {return mYMax;}
+    float minimumZ() const {return mZMin;}
+    float maximumZ() const {return mZMax;}
+
+    float length() const{
+        return std::abs(mXMax - mXMin);
+    }
+
+    float width() const{
+        return std::abs(mYMax - mYMin);
+    }
+
+    float height() const{
+        return std::abs(mZMax - mZMin);
+    }
+
+
+private:
+    float mXMin = 0.0f;
+    float mXMax = 0.0f;
+    float mYMin = 0.0f;
+    float mYMax = 0.0f;
+    float mZMin = 0.0f;
+    float mZMax = 0.0f;
+};
+
 /**
  * @brief Класс объекта, представленного набором вершин
  */
@@ -80,11 +120,46 @@ public:
      */
     float minimumZ() const;
 
+    /**
+     * @brief Возвращает минимальное значение вершины по оси x.
+     * @return Минимальное значение вершины по оси x.
+     */
+    float maximumX() const;
+
+    /**
+     * @brief Возвращает минимальное значение вершины по оси x.
+     * @return Минимальное значение вершины по оси x.
+     */
+    float minimumX() const;
+
+    /**
+     * @brief Возвращает минимальное значение вершины по оси y.
+     * @return Минимальное значение вершины по оси y.
+     */
+    float maximumY() const;
+
+    /**
+     * @brief Возвращает минимальное значение вершины по оси y.
+     * @return Минимальное значение вершины по оси y.
+     */
+    float minimumY() const;
+
+    Cube bounds() const;
+
+protected:
+
+    void createBounds();
+
 protected:
 
     int mPrimitiveType;        //< Тип примитива для отображения в движке openGL (из набора дефайнов gl.h).
     QVector <QVector3D> mData; //< Набор вершин объекта.
+    Cube mBounds = Cube(0.0f, 0.0f, 0.0f,0.0f,0.0f,0.0f);
     float mMaximumZ = 0.0f;
     float mMinimumZ = 0.0f;;
+    float mMaximumX = 0.0f;
+    float mMinimumX = 0.0f;;
+    float mMaximumY = 0.0f;
+    float mMinimumY = 0.0f;;
 
 };
