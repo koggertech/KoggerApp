17c9a6c34c96126b3cdaeaf354dca55fcc7072db
Kharlamenko <kharlamenkodev@gmail.com>
2023-05-28 16:31:43 +0300

    Cube class has been moved to own file

--- Core/vertexobject.h
+++ Core/vertexobject.h
@@ -12,46 +12,7 @@
 
 #include <GL/gl.h>
 
-class Cube{
-
-public:
-    Cube(float x_1, float x_2,
-             float y_1, float y_2,
-             float z_1, float z_2)
-    :mXMin(x_1), mXMax(x_2)
-    ,mYMin(y_1), mYMax(y_2)
-    ,mZMin(z_1), mZMax(z_2)
-    {}
-
-    float minimumX() const {return mXMin;}
-    float maximumX() const {return mXMax;}
-    float minimumY() const {return mYMin;}
-    float maximumY() const {return mYMax;}
-    float minimumZ() const {return mZMin;}
-    float maximumZ() const {return mZMax;}
-
-    float length() const{
-        return std::abs(mXMax - mXMin);
-    }
-
-    float width() const{
-        return std::abs(mYMax - mYMin);
-    }
-
-    float height() const{
-        return std::abs(mZMax - mZMin);
-    }
-
-
-private:
-    float mXMin = 0.0f;
-    float mXMax = 0.0f;
-    float mYMin = 0.0f;
-    float mYMax = 0.0f;
-    float mZMin = 0.0f;
-    float mZMax = 0.0f;
-};
-
+#include <cube.h>
 /**
  * @brief Класс объекта, представленного набором вершин
  */
