464db50051be4356d7c45c0733bf5e1aeea954f1
Kharlamenko <kharlamenkodev@gmail.com>
2023-07-25 06:46:31 +0300

    Inheritance changed to composition

--- domain/raycastpointpicker.cpp
+++ domain/raycastpointpicker.cpp
@@ -1,24 +1,41 @@
 #include "raycastpointpicker.h"
 
 RayCastPointPicker::RayCastPointPicker(const QVector3D& origin, const QVector3D& dir)
-: RayCastPolygonPicker(origin, dir)
 {
-
+    mpPolygonPicker = std::make_shared <RayCastPolygonPicker>(origin,dir);
+    mpLinePicker = std::make_shared <RayCastLinePicker>(origin,dir);
 }
 
 VertexObject RayCastPointPicker::pick(const VertexObject& object)
 {
-    auto pickedObject = RayCastPolygonPicker::pick(object);
+    VertexObject pickedObject(GL_POINTS, {});
+    QVector3D lastIntersectionPoint;
+
+    if (object.primitiveType() == GL_LINES ||
+        object.primitiveType() == GL_LINE_STRIP){
+        pickedObject = mpLinePicker->pick(object);
+        lastIntersectionPoint = mpLinePicker->lastIntersectionPoint();
+
+
+        return {GL_POINTS, {lastIntersectionPoint}};
+    }
+
+    if (object.primitiveType() == GL_TRIANGLES ||
+        object.primitiveType() == GL_QUADS)
+    {
+        pickedObject = mpPolygonPicker->pick(object);
+        lastIntersectionPoint = mpPolygonPicker->lastIntersectionPoint();
+    }
 
     if (pickedObject.cdata().isEmpty()){
         return {GL_POINTS, {}};
     }
 
     auto pickedPoint = pickedObject.cdata().first();
-    auto distToLastPoint = pickedPoint.distanceToPoint(mLastIntersectionPoint);
+    auto distToLastPoint = pickedPoint.distanceToPoint(lastIntersectionPoint);
 
     for (const auto& point : pickedObject.cdata()){
-        auto distToCurrentPoint = point.distanceToPoint(mLastIntersectionPoint);
+        auto distToCurrentPoint = point.distanceToPoint(lastIntersectionPoint);
 
         if (distToCurrentPoint <= distToLastPoint){
             pickedPoint = point;
--- domain/raycastpointpicker.h
+++ domain/raycastpointpicker.h
@@ -2,8 +2,9 @@
 #define RAYCASTPOINTPICKER_H
 
 #include "raycastpolygonpicker.h"
+#include "raycastlinepicker.h"
 
-class RayCastPointPicker : public RayCastPolygonPicker
+class RayCastPointPicker : public AbstractPicker
 {
 public:
     RayCastPointPicker(const QVector3D& origin, const QVector3D& dir);
@@ -11,6 +12,12 @@ public:
     VertexObject pick(const VertexObject& object) override;
 
     QString pickingMethod() override;
+
+private:
+
+    std::shared_ptr <RayCastPolygonPicker> mpPolygonPicker;
+    std::shared_ptr <RayCastLinePicker> mpLinePicker;
+
 };
 
 #endif // RAYCASTPOINTPICKER_H
