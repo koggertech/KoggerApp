35b7cc9298fc524f3bfee3eb6affb993eaa471ea
Kharlamenko <kharlamenkodev@gmail.com>
2023-09-25 09:59:11 +0300

    created qml component controllers base class

---
+++ Core/QmlObjectNames.h
@@ -0,0 +1,16 @@
+#ifndef QMLOBJECTNAMES_H
+#define QMLOBJECTNAMES_H
+
+#include <QString>
+
+namespace QmlObjectNames{
+    static const QString activeObjectParamsMenuLoaderName = "activeObjectParamsMenuLoader";
+    static const QString bottomTrackControlMenu           = "bottomTrackControlMenu";
+    static const QString surfaceControlMenu               = "surfaceControlMenu";
+    static const QString npdFilterControlMenu             = "npdFilterControlMenu";
+    static const QString mpcFilterControlMenu             = "mpcFilterControlMenu";
+    static const QString bottomTrackParamsMenu            = "bottomTrackParamsMenu";
+    static const QString surfaceParamsMenu                = "surfaceParamsMenu";
+}
+
+#endif // QMLOBJECTNAMES_H
