c8e419e09dfb6e655b0d44ac48043da10d4e5186
Kharlamenko <kharlamenkodev@gmail.com>
2023-09-25 10:04:51 +0300

    Created files with drawing helpers

---
+++ Core/drawutils.h
@@ -0,0 +1,11 @@
+#ifndef DRAWUTILS_H
+#define DRAWUTILS_H
+
+class QVector4D;
+class QColor;
+namespace DrawUtils
+{
+    QVector4D colorToVector4d(const QColor& color);
+};
+
+#endif // DRAWUTILS_H
