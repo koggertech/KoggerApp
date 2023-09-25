ad1ea8b8d2f9c0d563882e4db690275d386658de
Kharlamenko <kharlamenkodev@gmail.com>
2023-09-25 10:06:00 +0300

    Created scene object base class

--- Core/sceneobject.cpp
+++ Core/sceneobject.cpp
@@ -1,50 +1,164 @@
 #include "sceneobject.h"
 
 SceneObject::SceneObject(QObject *parent)
-    : QObject(parent)
-{
+: QObject(parent)
+{}
 
-}
-
-SceneObject::SceneObject(QString name, QObject *parent)
-    : QObject(parent)
-    , mName(name)
+SceneObject::SceneObject(QString name,
+                         int primitiveType,
+                         QObject *parent)
+: QObject(parent)
+, m_name(name)
+, m_primitiveType(primitiveType)
 {}
 
 SceneObject::~SceneObject()
 {}
 
-void SceneObject::draw(QOpenGLFunctions* ctx, const QMatrix4x4& mvp,  QMap <QString, QOpenGLShaderProgram*> shaderProgramMap) const
-{
-    Q_UNUSED(ctx)
-    Q_UNUSED(mvp)
-    Q_UNUSED(shaderProgramMap)
-}
-
 void SceneObject::setName(QString name)
 {
-    if(mName == name)
+    if(m_name != name)
+        m_name = name;
+}
+
+void SceneObject::setScene(GraphicsScene3d* scene)
+{
+    if(mp_scene == scene)
         return;
 
-    mName = name;
+    mp_scene = scene;
+}
 
-    Q_EMIT nameChanged(mName);
+GraphicsScene3d* SceneObject::scene() const
+{
+    return mp_scene;
 }
 
 QString SceneObject::id() const
 {
-    return mUuid.toString(QUuid::WithoutBraces);
+    return m_uuid.toString(QUuid::WithoutBraces);
 }
 
 QString SceneObject::name() const
 {
-    return mName;
+    return m_name;
+}
+
+QVector<QVector3D> SceneObject::data() const
+{
+    return m_data;
+}
+
+const QVector<QVector3D> &SceneObject::cdata() const
+{
+    return m_data;
+}
+
+AbstractEntityDataFilter *SceneObject::filter() const
+{
+    return m_filter.get();
+}
+
+void SceneObject::setData(const QVector<QVector3D> &data)
+{
+    if(m_filter){
+        QVector <QVector3D> filteredData;
+        m_filter->apply(data, filteredData);
+        m_data = filteredData;
+        return;
+    }
+
+    m_data = data;
+
+    createBoundingBox();
+}
+
+void SceneObject::setFilter(std::shared_ptr<AbstractEntityDataFilter> filter)
+{
+    if(m_filter == filter)
+        return;
+
+    m_filter = filter;
+}
+
+void SceneObject::clearData()
+{
+    m_data.clear();
+}
+
+void SceneObject::setPrimitiveType(int primitiveType)
+{
+    if(m_primitiveType != primitiveType)
+        m_primitiveType = primitiveType;
+}
+
+void SceneObject::append(const QVector3D &vertex)
+{
+    m_data.append(vertex);
+
+    Q_EMIT dataChanged();
+
+    createBoundingBox();
+
+    Q_EMIT boundsChanged();
+}
+
+void SceneObject::append(const QVector<QVector3D> &other)
+{
+    m_data.append(other);
+
+    Q_EMIT dataChanged();
+
+    createBoundingBox();
+
+    Q_EMIT boundsChanged();
+}
+
+int SceneObject::primitiveType() const
+{
+    return m_primitiveType;
+}
+
+Cube SceneObject::boundingBox() const
+{
+    return m_boundingBox;
 }
 
 void SceneObject::qmlDeclare()
 {
     qmlRegisterUncreatableType <SceneObject> ("SceneObject", 1, 0, "SceneObject", "");
     qRegisterMetaType<SceneObject::SceneObjectType>("SceneObjectType");
+    qRegisterMetaType<AbstractEntityDataFilter::FilterType>("FilterType");
+}
+
+void SceneObject::createBoundingBox()
+{
+    if (m_data.isEmpty()){
+        m_boundingBox = Cube(0.0f, 0.0f, 0.0f,0.0f,0.0f,0.0f);
+        return;
+    }
+
+    float z_max = m_data.first().z();
+    float z_min = z_max;
+
+    float x_max = m_data.first().x();
+    float x_min = x_max;
+
+    float y_max = m_data.first().y();
+    float y_min = y_max;
+
+    for (const auto& v: m_data){
+        z_min = std::min(z_min, v.z());
+        z_max = std::max(z_max, v.z());
+
+        x_min = std::min(x_min, v.x());
+        x_max = std::max(x_max, v.x());
+
+        y_min = std::min(y_min, v.y());
+        y_max = std::max(y_max, v.y());
+    }
+
+    m_boundingBox = Cube(x_min, x_max, y_min, y_max, z_min, z_max);
 }
 
 SceneObject::SceneObjectType SceneObject::type() const
--- Core/sceneobject.h
+++ Core/sceneobject.h
@@ -8,18 +8,22 @@
 #include <QOpenGLFunctions>
 #include <QOpenGLShaderProgram>
 
+#include <abstractentitydatafilter.h>
+#include <cube.h>
+
+class GraphicsScene3d;
 class SceneObject : public QObject
 {
     Q_OBJECT
-
-    Q_PROPERTY(QString         name READ name WRITE setName NOTIFY nameChanged)
-    Q_PROPERTY(SceneObjectType type READ type               CONSTANT)
+    Q_PROPERTY(AbstractEntityDataFilter* filter READ filter                 CONSTANT)
+    Q_PROPERTY(QString                   name   READ name   WRITE setName   NOTIFY nameChanged)
+    Q_PROPERTY(SceneObjectType           type   READ type                   CONSTANT)
 
 public:
-
-    explicit SceneObject(QObject *parent = nullptr);
+    SceneObject(QObject *parent = nullptr);
 
     SceneObject(QString name = QStringLiteral("Scene object"),
+                int primitiveType = GL_POINTS,
                 QObject *parent = nullptr);
 
     virtual ~SceneObject();
@@ -29,7 +33,6 @@ public:
         Surface      = 1,
         Point        = 2,
         Polygon      = 3,
-        ObjectsGroup = 4,
         PointGroup   = 5,
         PolygonGroup = 6,
         Unknown      = 7
@@ -37,21 +40,7 @@ public:
 
     Q_ENUM(SceneObjectType)
 
-    virtual void draw(QOpenGLFunctions* ctx,
-                      const QMatrix4x4& mvp,
-                       QMap <QString, QOpenGLShaderProgram*> shaderProgramMap) const;
-
-    /**
-     * @brief Returns type of the object
-     * @return type of the object
-     */
-    virtual SceneObjectType type() const;
-
-    /**
-     * @brief Sets the name of object
-     * @param[in] name - name of object
-     */
-    void setName(QString name);
+    GraphicsScene3d* scene() const;
 
     /**
      * @brief Returns unique id of the object
@@ -60,24 +49,84 @@ public:
     QString id() const;
 
     /**
+     * @brief Returns type of the object
+     * @return type of the object
+     */
+    virtual SceneObjectType type() const;
+
+    /**
      * @brief Returns name of the object
      * @return name of the object
      */
     QString name() const;
 
+    //! @brief Возвращает копию набора вершин объекта.
+    //! @return Копмя набора вершин объекта.
+    QVector <QVector3D> data() const;
+
+    //! @brief Возвращает копию набора вершин объекта.
+    //! @return Копмя набора вершин объекта.
+    const QVector <QVector3D>& cdata() const;
+
+    //! @brief Возвращает тип примитива для отображения в движке openGL
+    //! (из набора дефайнов gl.h).
+    //! @return[in] Тип примитива для отображения в движке openGL
+    //! (из набора дефайнов gl.h).
+    int primitiveType() const;
+
+    Cube boundingBox() const;
+
+    AbstractEntityDataFilter* filter() const;
+
     static void qmlDeclare();
 
-signals:
+protected:
+    void createBoundingBox();
 
+public Q_SLOTS:
     /**
-     * @brief Emits when object name was changed
-     * @param[out] name Object name
+     * @brief Sets the name of object
+     * @param[in] name - name of object
      */
+    void setName(QString name);
+
+    void setScene(GraphicsScene3d* scene);
+
+    //! @brief Устанавливает набор вершин объекта.
+    //! @param[in] data - ссылка на набор вершин.
+    virtual void setData(const QVector <QVector3D>& data);
+
+    virtual void clearData();
+
+    virtual void setPrimitiveType(int primitiveType);
+
+    void setFilter(std::shared_ptr <AbstractEntityDataFilter> filter);
+
+    //! @brief Добавляет вершину в конец набора вершин.
+    //! @param[in] vertex - ссылка на вершину
+    virtual void append(const QVector3D& vertex);
+
+    //! @brief Добавляет входящий набор вершин в конец набора вершин объекта
+    //! @param[in] other - ссылка на набор вершин
+    virtual void append(const QVector<QVector3D>& other);
+
+Q_SIGNALS:
+    void dataChanged();
+
     void nameChanged(QString name);
 
+    void boundsChanged();
+
+    void filterChanged(AbstractEntityDataFilter* filter);
+
 protected:
-    QString mName = QStringLiteral("Scene object"); ///< Object name
-    QUuid mUuid   = QUuid::createUuid();            ///< Object unique id
+    GraphicsScene3d* mp_scene;
+    QString m_name = QStringLiteral("Scene object"); ///< Object name
+    QUuid m_uuid   = QUuid::createUuid();            ///< Object unique id
+    QVector <QVector3D> m_data;
+    Cube m_boundingBox;                             ///< Bounding cube of the object
+    int m_primitiveType = GL_POINTS;
+    std::shared_ptr <AbstractEntityDataFilter> m_filter;
 };
 
 #endif // SCENEOBJECT_H
