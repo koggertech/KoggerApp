12ad1e4a7a0bb0dd5f69584d9b5d98b75477c25f
Kharlamenko <kharlamenkodev@gmail.com>
2023-09-25 10:58:26 +0300

    base class changed

--- domain/contour.h
+++ domain/contour.h
@@ -1,9 +1,9 @@
 #ifndef CONTOUR_H
 #define CONTOUR_H
 
-#include <displayedobject.h>
+#include <scenegraphicsobject.h>
 
-class Contour : public DisplayedObject
+class Contour : public SceneGraphicsObject
 {
     Q_OBJECT
 
@@ -18,14 +18,14 @@ public:
 
     QVector4D keyPointsColor() const;
 
+public Q_SLOTS:
     void setKeyPointsVisible(bool visible);
 
     void setKeyPointsColor(QColor color);
 
 protected:
-
-    bool   mKeyPointsVisible = false;
-    QColor mKeyPointsColor = QColor(255.0f, 255.0f, 255.0f, 255.0f);
+    bool   m_keyPointsVisible = false;
+    QColor m_keyPointsColor = QColor(255.0f, 255.0f, 255.0f, 255.0f);
 };
 
 #endif // CONTOUR_H
