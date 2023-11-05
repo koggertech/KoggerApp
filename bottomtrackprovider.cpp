6329c533261c49093f4be7b11122d5e1796466e3
Kharlamenko <kharlamenkodev@gmail.com>
2023-11-05 19:43:28 +0300

    Converting data from z-up axis coordinate system to y-up (opengl)

--- bottomtrackprovider.cpp
+++ bottomtrackprovider.cpp
@@ -1,8 +1,14 @@
 #include "bottomtrackprovider.h"
 
+#include <QMatrix4x4>
+
 void BottomTrackProvider::setBottomTrack(QVector<QVector3D> &bottomTrack)
 {
-    m_bottomTrack = bottomTrack;
+    //m_bottomTrack = bottomTrack;
+    m_bottomTrack.clear();
+
+    for(auto& v : bottomTrack)
+        m_bottomTrack.append(QVector3D(v.x(), v.z(), -v.y()));
 
     Q_EMIT bottomTrackChanged(m_bottomTrack);
 }
