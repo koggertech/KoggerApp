3ddc43099897824c21ca9142eb69bcd25b12041b
Kharlamenko <kharlamenkodev@gmail.com>
2023-11-12 13:50:53 +0300

    Implemented vertex selection selection mode behaviour

--- domain/bottomtrack.cpp
+++ domain/bottomtrack.cpp
@@ -1,7 +1,8 @@
 #include "bottomtrack.h"
+#include <graphicsscene3dview.h>
 
-BottomTrack::BottomTrack(QObject* parent)
-    : SceneObject(new BottomTrackRenderImplementation, parent)
+BottomTrack::BottomTrack(GraphicsScene3dView* view, QObject* parent)
+    : SceneObject(new BottomTrackRenderImplementation,view,parent)
 {}
 
 BottomTrack::~BottomTrack()
@@ -22,6 +23,68 @@ void BottomTrack::setData(const QVector<QVector3D> &data, int primitiveType)
     }
 
     SceneObject::setData(data, primitiveType);
+}
+
+void BottomTrack::resetVertexSelection()
+{
+    RENDER_IMPL(BottomTrack)->m_selectedVertexIndices.clear();
+}
+
+void BottomTrack::mouseMoveEvent(Qt::MouseButtons buttons, qreal x, qreal y)
+{
+    if(!m_view) return;
+
+    switch(m_view->m_mode){
+    case GraphicsScene3dView::BottomTrackVertexSelectionMode:{
+        auto hits = m_view->m_ray.hitObject(shared_from_this(), Ray::HittingMode::Vertex);
+        if(!hits.isEmpty())
+            RENDER_IMPL(BottomTrack)->m_selectedVertexIndices = {hits.first().indices().first};
+    }
+    break;
+    case GraphicsScene3dView::BottomTrackVertexComboSelectionMode:
+    {
+
+    }
+    break;
+    case GraphicsScene3dView::BottomTrackSyncPointCreationMode:
+    {
+
+    }
+    break;
+    default:
+        qDebug() << "Nothing to do for bottom track ((";
+        RENDER_IMPL(BottomTrack)->m_selectedVertexIndices.clear();
+        break;
+    }
+}
+
+void BottomTrack::mousePressEvent(Qt::MouseButtons buttons, qreal x, qreal y)
+{
+    if(!m_view) return;
+}
+
+void BottomTrack::mouseReleaseEvent(Qt::MouseButtons buttons, qreal x, qreal y)
+{
+    if(!m_view) return;
+}
+
+void BottomTrack::keyPressEvent(Qt::Key key)
+{
+    if(!m_view) return;
+
+    switch(m_view->m_mode){
+    case GraphicsScene3dView::BottomTrackVertexSelectionMode:
+    {
+        if(key == Qt::Key_Delete){
+            auto indices = RENDER_IMPL(BottomTrack)->m_selectedVertexIndices;
+            for(const auto& i : indices)
+                removeVertex(i);
+        }
+    }
+    break;
+    default:
+        break;
+    }
 }
 
 //-----------------------RenderImplementation-----------------------------//
@@ -71,4 +134,31 @@ void BottomTrack::BottomTrackRenderImplementation::render(QOpenGLFunctions *ctx,
 
     shaderProgram->disableAttributeArray(posLoc);
     shaderProgram->release();
+
+    //------------->Drawing selected vertices<<---------------//
+    shaderProgram = shaderProgramMap["static"];
+    shaderProgram->bind();
+
+    colorLoc  = shaderProgram->uniformLocation("color");
+    matrixLoc = shaderProgram->uniformLocation("matrix");
+    posLoc    = shaderProgram->attributeLocation("position");
+    int widthLoc  = shaderProgram->uniformLocation("width");
+
+    QVector4D vertexColor(1.0f, 0.0f, 0.0f, 1.0f);
+    QVector<QVector3D> selectedVertices;
+    for(const auto& i : m_selectedVertexIndices)
+        selectedVertices.append(m_data.at(i));
+
+    shaderProgram->setUniformValue(colorLoc,vertexColor);
+    shaderProgram->setUniformValue(matrixLoc, mvp);
+    shaderProgram->setUniformValue(widthLoc, 10.0f);
+    shaderProgram->enableAttributeArray(posLoc);
+    shaderProgram->setAttributeArray(posLoc, selectedVertices.constData());
+
+    ctx->glEnable(GL_PROGRAM_POINT_SIZE);
+    ctx->glLineWidth(4.0);
+    ctx->glDrawArrays(GL_POINTS, 0, selectedVertices.size());
+    ctx->glLineWidth(1.0);
+    ctx->glDisable(GL_PROGRAM_POINT_SIZE);
+    //------------->
 }
--- domain/bottomtrack.h
+++ domain/bottomtrack.h
@@ -4,6 +4,7 @@
 #include <memory>
 #include <sceneobject.h>
 
+class GraphicsScene3dView;
 class BottomTrack : public SceneObject
 {
     Q_OBJECT
@@ -18,12 +19,24 @@ public:
         virtual void render(QOpenGLFunctions* ctx,
                             const QMatrix4x4& mvp,
                             const QMap <QString, std::shared_ptr <QOpenGLShaderProgram>>& shaderProgramMap) const override;
+        QVector<int> m_selectedVertexIndices;
     };
 
-    explicit BottomTrack(QObject* parent = nullptr);
+    explicit BottomTrack(GraphicsScene3dView* view = nullptr, QObject* parent = nullptr);
     virtual ~BottomTrack();
     virtual SceneObjectType type() const override;
+
+public Q_SLOTS:
     virtual void setData(const QVector<QVector3D>& data, int primitiveType = GL_POINTS) override;
+    void resetVertexSelection();
+
+protected:
+    friend class GraphicsScene3dView;
+
+    virtual void mouseMoveEvent(Qt::MouseButtons buttons, qreal x, qreal y) override;
+    virtual void mousePressEvent(Qt::MouseButtons buttons, qreal x, qreal y) override;
+    virtual void mouseReleaseEvent(Qt::MouseButtons buttons, qreal x, qreal y) override;
+    virtual void keyPressEvent(Qt::Key key) override;
 };
 
 #endif // BOTTOMTRACK_H
