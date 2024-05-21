cfed59611cf0138a1433c9c1fc896eeb16bbb94f
Evgeniy Saltykov <evgeniysaltykov38@gmail.com>
2024-05-21 15:23:06 +0400

    auto project packing when using deploy tool
    Signed-off-by: Evgeniy Saltykov <evgeniysaltykov38@gmail.com>

--- deploy_win_x64_release.bat
+++ deploy_win_x64_release.bat
@@ -2,15 +2,21 @@
 REM locat paths:
 set binPath=build\MinGW_8_1_0_x64_desktop-Release\release\KoggerApp.exe
 set qmlPath=QML
-set outPath=build\MinGW_8_1_0_x64_desktop-Release\out_x64
+set outPath=out_x64
+set zip_file=out_x64\out_x64.zip
+
 echo executable file path: "%binPath%"
 echo QML project files path: "%qmlPath%"
 echo output path: "%outPath%"
+
 if exist "%outPath%" (
     rmdir /s /q "%outPath%"
     echo the output directory will be overwritten
 )
-REM deploy and cope binary file
+REM deploy and copy binary file:
 windeployqt %binPath% -qmldir %qmlPath% -dir %outPath% -no-translations -no-virtualkeyboard
 xcopy "%binPath%" "%outPath%"
+
+REM .zip archiving
+7z a "%zip_file%" "%outPath%" %compression_level%
 pause
