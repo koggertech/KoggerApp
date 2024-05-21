d7a79e31819edbae610d7246b5d2484b37d5f000
Evgeniy Saltykov <evgeniysaltykov38@gmail.com>
2024-05-21 16:06:22 +0400

    .zip file name changed (win deploy)
    Signed-off-by: Evgeniy Saltykov <evgeniysaltykov38@gmail.com>

--- deploy_win_x64_release.bat
+++ deploy_win_x64_release.bat
@@ -4,7 +4,7 @@ REM local paths:
 set binPath=build\MinGW_8_1_0_x64_desktop-Release\release\KoggerApp.exe
 set qmlPath=QML
 set outPath=out_x64
-set zip_file=out_x64\koggerApp_x64.zip
+set zip_file=out_x64\KoggerApp_win_x64.zip
 echo executable file path: "%binPath%"
 echo QML project files path: "%qmlPath%"
 echo output path: "%outPath%"
