c36c25ec20b7c7349630b8227049d7fc9672554d
Evgeniy Saltykov <evgeniysaltykov38@gmail.com>
2024-05-21 15:49:36 +0400

    change dir for compressing
    Signed-off-by: Evgeniy Saltykov <evgeniysaltykov38@gmail.com>

--- deploy_win_x64_release.bat
+++ deploy_win_x64_release.bat
@@ -1,22 +1,25 @@
 @echo off
-REM locat paths:
+
+REM local paths:
 set binPath=build\MinGW_8_1_0_x64_desktop-Release\release\KoggerApp.exe
 set qmlPath=QML
 set outPath=out_x64
-set zip_file=out_x64\out_x64.zip
-
+set zip_file=out_x64\koggerApp_x64.zip
 echo executable file path: "%binPath%"
 echo QML project files path: "%qmlPath%"
 echo output path: "%outPath%"
-
 if exist "%outPath%" (
     rmdir /s /q "%outPath%"
     echo the output directory will be overwritten
 )
+
 REM deploy and copy binary file:
 windeployqt %binPath% -qmldir %qmlPath% -dir %outPath% -no-translations -no-virtualkeyboard
 xcopy "%binPath%" "%outPath%"
 
 REM .zip archiving
-7z a "%zip_file%" "%outPath%" %compression_level%
+pushd "%outPath%"
+7z a "..\%zip_file%" * %compression_level%
+popd
+
 pause
